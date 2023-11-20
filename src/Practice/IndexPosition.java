51c0d95b2b7e2a28aced41c383d307b54d6f86f4
Hema <hemajeya004@gmail.com>
2023-11-20 13:42:27 +0530

    queue

--- src/Practice/IndexPosition.java
+++ src/Practice/IndexPosition.java
@@ -1,5 +1,17 @@
 package Practice;
 
 public class IndexPosition {
+	public static void main(String[] args) {
+		String str = "Power";
+        char charToFind = 'o';
+
+        int index = str.indexOf(charToFind);
+
+        if (index != -1) {
+            System.out.println(+index);
+        } else {
+            System.out.println(+index);
+        }
+	}
 
 }
